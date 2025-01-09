9b53c41d5975962c2d8229dd1f93a60617f71837
same9neko10 <same9neko10@gmail.com>
2025-01-09 08:55:38 +0000

    20250109

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 // Add JS here
 const startscreen = document.getElementById("startscreen");
+const startscrean_botton = document.getElementById("startscrean_botton");
+const OP_1 = document.getElementById("OP_1");
+
+//ボタンが押されたら表示非表示　（スタート画面からあらすじ画面1 すすむ）
+startscrean_botton.addEventListener("click", e => {
+    startscreen.style.display = "none"
+    OP_1.style.display = "block"
+}, false);
